1d2657c5d117afe8e3d600a88fafa2ed988f7d26
wengxingxia <15088132621@163.com>
2017-08-15 18:35:54 +0800

    欢迎页初始

---
+++ app/src/main/java/com/example/wxx/thequeenofspades/WelcomeActivity.java
@@ -0,0 +1,22 @@
+package com.example.wxx.thequeenofspades;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //取消状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_welcome);
+
+    }
+}
