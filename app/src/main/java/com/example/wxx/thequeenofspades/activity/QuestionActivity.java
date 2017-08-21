44653d90a0adf2cfdd719887e8f0dbeec6d2a8a5
wengxingxia <15088132621@163.com>
2017-08-21 19:14:08 +0800

    加入四叶草图片

---
+++ app/src/main/java/com/example/wxx/thequeenofspades/activity/QuestionActivity.java
@@ -0,0 +1,15 @@
+package com.example.wxx.thequeenofspades.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.wxx.thequeenofspades.R;
+
+public class QuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+    }
+}
